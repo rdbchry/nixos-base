09ca8d7fdcd99c2a6ec8cbb811e2f094e55deea7
rdbchry <120248013+rdbchry@users.noreply.github.com>
2023-12-23 06:52:39 +0800

    Delete system directory

--- system/hardware/kernel.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    zenpower
-    amdgpu-pro
-  ];
-}
--- system/hardware/systemd.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  services.journald.extraConfig = "SystemMaxUse=250M\nSystemMaxFiles=10";
-  services.journald.rateLimitBurst = 800;
-  services.journald.rateLimitInterval = "5s";
-}
